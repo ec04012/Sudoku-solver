78f69e72e8c9b15dbe7889dd89b6db6a2e7d0dc5
ec04012 <ec04012@uga.edu>
2020-08-09 01:17:11 -0400

    Removed debugging and print statements

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -32,7 +32,6 @@ bool isValid(int y, int x, int n) {
     // Check Column
     for (int i = 0; i < size; i++) {
         if (matrix[i][x] == n) {
-            cout << "dupe in col" << endl;
             return false;
         }
     }
@@ -40,7 +39,6 @@ bool isValid(int y, int x, int n) {
     // Check Row
     for (int i = 0; i < size; i++) {
         if (matrix[y][i] == n) {
-            cout << "dupe in row" << endl;
             return false;
         }
     }
@@ -52,7 +50,6 @@ bool isValid(int y, int x, int n) {
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             if (matrix[y0 * 3 + i][x0 * 3 + j] == n) {
-                cout << "dupe in sub grid" << endl;
                 return false;
             }
         }
@@ -78,23 +75,13 @@ void printMatrix() {
 }
 
 void solve() {
-    if (counter > 4210) {
-        exit(1);
-    }
-
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
-            if (matrix[i][j] == 0) {
-                cout << "Filling position: " << i << "," << j << endl;                
+            if (matrix[i][j] == 0) {              
                 for (int n = 1; n <= 9; n++) {
-                    cout << "trying " << n << " in " << i << "," << j << endl;
                     if (isValid(i, j, n)) {
-                        cout << "Filling position: " << i << "," << j << " with " << n << endl; 
                         matrix[i][j] = n;
-                        printMatrix();
-                        counter++;
                         solve();
-                        cout << "Backtrack" << endl;
                         matrix[i][j] = 0;
                     }
                 }
@@ -108,13 +95,9 @@ void solve() {
     exit(0);
 }
 
-void testValid(int y, int x, int n) {
-    cout << n << " at (" << y << "," << x << ")" << endl;
-    printMatrix();
-    cout << isValid(y, x, n) << endl;
-}
-
 int main() {
+    cout << "Initial puzzle" << endl;
+    printMatrix();
     solve();
     return 0;
 }
